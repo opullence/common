a1cc71ab22567f325f395c919d4bbe11c73fc7d9
jurelou <louis@jurczyk.fr>
2019-10-30 22:13:44 +0100

    v0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements/production.txt") as f:
 
 setup(
     name="opulence.common",
-    version="0.0.1",
+    version="0.0.2",
     description="Common tools",
     long_description=readme,
     author="Opulence",
