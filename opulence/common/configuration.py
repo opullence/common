357b9612476c406f530d0ff8baa37c35dfedc431
jurelou <louis@jurczyk.fr>
2019-11-01 18:06:41 +0100

    common: set logger

--- opulence/common/configuration.py
+++ opulence/common/configuration.py
@@ -1,3 +1,4 @@
+import logging.config
 import os
 
 import yaml
@@ -5,6 +6,10 @@ from celery import Celery
 from kombu.serialization import register
 
 from . import jsonEncoder
+
+CONFIG_FILENAME = os.getenv("CONFIG_FILE", "settings.yml")
+config = {}
+
 
 register(
     "customEncoder",
@@ -14,24 +19,16 @@ register(
     content_encoding="utf-8",
 )
 
-config = {}
 
-
-def load_config_from_file(filename):
-    global config
-
-    module_path = os.path.abspath(__file__)
-    for loc in os.path.dirname(module_path), os.curdir, os.path.expanduser("~"):
-        try:
-            with open(os.path.join(loc, filename), "r") as stream:
-                config = yaml.safe_load(stream)
-            break
-        except yaml.YAMLError as err:
-            print("Configuration format error:", err)
-        except IOError:
-            pass
-        except Exception as err:
-            print("Error while trying to load configuration file", err)
+try:
+    with open(CONFIG_FILENAME, "r") as stream:
+        config = yaml.safe_load(stream)
+except yaml.YAMLError as err:
+    print("Configuration format error:", err)
+except IOError:
+    pass
+except Exception as err:
+    print("Error while trying to load configuration file", err)
 
 
 def configure_celery(config, **kwargs):
@@ -46,3 +43,45 @@ def configure_celery(config, **kwargs):
         }
     )
     return app
+
+
+class RequireDebugTrue(logging.Filter):
+    def filter(self, record):
+        return config["log"]["debug"]
+
+
+LOGFILE = os.path.join(os.getcwd(), "opulence.log")
+if "file" in config["log"]:
+    LOGFILE = config["log"]["file"]
+
+
+DEFAULT_LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "filters": {"require_debug_true": {"()": RequireDebugTrue}},
+    "formatters": {
+        "verbose": {
+            "format": "%(asctime)s %(levelname)s %(name)s %(thread)d %(threadName)s %(message)s"
+        },
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "filters": ["require_debug_true"],
+            "formatter": "simple",
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.handlers.WatchedFileHandler",
+            "filename": LOGFILE,
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "": {"handlers": ["console", "file"], "level": "INFO", "propagate": False}
+    },
+}
+
+logging.config.dictConfig(DEFAULT_LOGGING)
