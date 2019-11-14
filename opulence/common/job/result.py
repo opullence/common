5ecf5540866b52948d27ef1f9e519ca29d2c62a8
louis jurczyk <louis.jurczyk@epitech.eu>
2019-11-14 22:18:56 +0100

    Remove Composable from jsonEncoder

--- opulence/common/job/result.py
+++ opulence/common/job/result.py
@@ -101,18 +101,15 @@ class Result(JsonSerializable):
             }
 
     def to_json(self):
-        obj_dict = super().to_json()
-        obj_dict.update(
-            {
-                "_input": None,
-                "_output": None,
+        obj_dict = {
+                "__class__": self.__class__.__name__,
+                "__module__": self.__module__,
                 "identifier": self.identifier.hex,
                 "input": self.input.get(),
                 "output": self.output,
                 "clock": self.clock.to_json(),
                 "status": (int(self.status["status"]), self.status["error"])
-            }
-        )
+        }
         return obj_dict
 
     @staticmethod
--- opulence/common/jsonEncoder.py
+++ opulence/common/jsonEncoder.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from .facts import BaseFact
 from .fields import BaseField
-from .job import Composable, Result
+from .job import Result
 from .utils import datetime_to_str, str_to_datetime
 
 
@@ -13,8 +13,6 @@ class encode(json.JSONEncoder):
             return {"__type__": "__result__", "result": obj.to_json()}
         elif isinstance(obj, BaseFact):
             return {"__type__": "__basefact__", "fact": obj.to_json()}
-        elif isinstance(obj, Composable):
-            return {"__type__": "__composable__", "composable": obj.to_json()}
         elif isinstance(obj, BaseField):
             return {"__type__": "__basefield__", "field": obj.to_json()}
         elif isinstance(obj, datetime):
@@ -26,8 +24,6 @@ def decode(obj):
     if "__type__" in obj:
         if obj["__type__"] == "__result__":
             return Result.from_json(obj["result"])
-        elif obj["__type__"] == "__composable__":
-            return Composable.from_json(obj["composable"])
         elif obj["__type__"] == "__basefact__":
             return BaseFact.from_json(obj["fact"])
         elif obj["__type__"] == "__basefield__":
