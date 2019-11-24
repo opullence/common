540750d39da68a07b8fbe5a8a4a0615268b69dcd
louis jurczyk <louis.jurczyk@epitech.eu>
2019-11-24 15:21:01 +0100

    Serialize datetime

--- opulence/common/jsonEncoder.py
+++ opulence/common/jsonEncoder.py
@@ -1,8 +1,10 @@
 import json
+from datetime import datetime
 
 from .facts import BaseFact
 from .fields import BaseField
 from .job import Result
+from .utils import datetime_to_str, str_to_datetime
 
 
 class encode(json.JSONEncoder):
@@ -13,6 +15,8 @@ class encode(json.JSONEncoder):
             return {"__type__": "__basefact__", "fact": obj.to_json()}
         elif isinstance(obj, BaseField):
             return {"__type__": "__basefield__", "field": obj.to_json()}
+        elif isinstance(obj, datetime):
+            return {"__type__": "__datetime__", "epoch": datetime_to_str(obj)}
         return json.JSONEncoder.default(self, obj)  # pragma: no cover
 
 
@@ -24,6 +28,8 @@ def decode(obj):
             return BaseFact.from_json(obj["fact"])
         elif obj["__type__"] == "__basefield__":
             return BaseField.from_json(obj["field"])
+        elif obj["__type__"] == "__datetime__":
+            return str_to_datetime(obj["epoch"])
     return obj
 
 
